1a6b28c4f6ddcdc8245dec6ca9af0553f4e3b367
nomanmohammadofficial <nomanmohammadofficial@gmail.com>
2024-11-18 14:48:37 +0530

    Update server configuration and add start script for Render deployment

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,11 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', process.env.PORT || 1337),
+  url: env('PUBLIC_URL', 'https://nomogen-strapi.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
